339aa185896a2c56b3a5af80addeaf48cce1eb48
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-05-10 23:51:36 +0300

    Add files via upload

--- Online Dev Blog/src/app/core/header/header.component.ts
+++ Online Dev Blog/src/app/core/header/header.component.ts
@@ -1,46 +1,104 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { trigger, state, style, animate, transition, group } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        'max-height': '500px', 'opacity': '1', 'visibility': 'visible'
+      })),
+      state('out', style({
+        'max-height': '0px', 'opacity': '0', 'visibility': 'hidden'
+      })),
+      transition('in => out', [group([
+        animate('400ms ease-in-out', style({
+          'opacity': '0'
+        })),
+        animate('600ms ease-in-out', style({
+          'max-height': '0px'
+        })),
+        animate('700ms ease-in-out', style({
+          'visibility': 'hidden'
+        }))
+      ]
+      )]),
+      transition('out => in', [group([
+        animate('1ms ease-in-out', style({
+          'visibility': 'visible'
+        })),
+        animate('600ms ease-in-out', style({
+          'max-height': '500px'
+        })),
+        animate('800ms ease-in-out', style({
+          'opacity': '1'
+        }))
+      ]
+      )])
+    ]),
+    
+    trigger('colorChange', [
+      state('green', style({
+        'background-color': 'green'
+      })),
+      state('transparent', style({
+        'background-color': 'transparent'
+      })),
+      transition('green <=> transparent', [
+        animate('0.5s')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   isShowed = false;
-  headerHeight = "90px";
+  isGreen = false;
 
   @ViewChild('header') headerElement?: ElementRef;
   @ViewChild('h1') h1Element?: ElementRef;
-
   @ViewChild('img') imgElement?: ElementRef;
   @ViewChild('bio') bioElement?: ElementRef;
+  @ViewChild('hr') hrElement?: ElementRef;
+
   @ViewChild('socials') socialsElement?: ElementRef;
   @ViewChild('nav') navElement?: ElementRef;
 
   constructor() { }
 
-  showMenu() {
-    if (!this.isShowed && this.headerElement) {
-      console.log(this.isShowed);
-      this.headerElement!.nativeElement.style.display = 'none';
-// this.h1Element!.nativeElement.style.display = 'block'
-      // this.imgElement!.nativeElement.style.display = 'none'
-      // this.bioElement!.nativeElement.style.display = 'none'
-      // this.socialsElement!.nativeElement.style.display = 'none'
-      // // this.isShowed = !this.isShowed;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateMenuState(window.innerWidth);
+  }
 
-      // Implement your logic for showing the menu
+  updateMenuState(screenWidth: number) {
+    if (screenWidth > 1000) {
+      this.isShowed = true;
+    } else {
+      this.isShowed = false;
     }
-    else {
-      console.log(this.isShowed);
+  }
 
-      this.headerElement!.nativeElement.style.display = 'block';
-      // this.imgElement!.nativeElement.style.display = 'block'
-      // this.bioElement!.nativeElement.style.display = 'block'
-      // this.socialsElement!.nativeElement.style.display = 'inline-block'
-
-    }
+  toggleMenu() {
     this.isShowed = !this.isShowed;
+  }
 
+  get stateName() {
+    return this.isShowed ? 'in' : 'out';
+  }
+
+  get colorStateName() {
+    return this.isGreen ? 'green' : 'transparent';
+  }
+
+  showMenu() {
+    this.toggleMenu();
+    this.isGreen = this.isShowed && window.innerWidth <= 1000;
+    setTimeout(() => {
+      this.headerElement!.nativeElement.style.opacity = '1';
+      // this.headerElement!.nativeElement.style.height = 'auto'
+    }, 1400)
   }
 }
