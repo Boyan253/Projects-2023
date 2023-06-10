435f6aab6b631ccc5601a6412c5dc92fe9935a73
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-06-10 11:10:36 +0300

    Add files via upload

--- Online Portfolio/server/index.js
+++ Online Portfolio/server/index.js
@@ -4,22 +4,19 @@ start()
 
 
 async function start() {
+    const app = express()
 
-    app.set('views', path.join(__dirname, 'client', 'pages')); // Specify the directory where your views are located
+    // app.set('views', path.join("C:/Users/bobob/Desktop/MyPortfolio/", 'client/', 'pages')); // Specify the directory where your views are located
     app.set('view engine', 'jsx'); // Set the view engine to 'jsx'
     app.engine('jsx', require('express-react-views').createEngine()); // Use express-react-views as the view engine
 
 
 
-    const app = express()
     app.listen(process.env.PORT, () => console.log(`server is listening on port ${process.env.PORT}`))
 
 }
 
 
 
-app.get('/', (req, res) => {
-
-    res.send('hello world!')
-})
+{/* <issue_comment>username_1: @username_0 I'm not sure what you're trying to do. */}
 
